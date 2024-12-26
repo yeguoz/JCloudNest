69ab1d1749f86ec746afdc778576f130572c97f0
yeguoz <aidjajd@163.com>
2024-12-26 15:57:08 +0800

    "spring Initialization"

---
+++ src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
@@ -0,0 +1,13 @@
+package icu.yeguo.cloudnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icu/yeguo/cloudnest/CloudNestApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.yeguo.cloudnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
