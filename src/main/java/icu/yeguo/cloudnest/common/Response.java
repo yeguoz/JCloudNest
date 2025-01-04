01516e113e34bb5bfd81c00b80cd506db3cd347b
yeguoz <aidjajd@163.com>
2025-01-04 22:49:24 +0800

    refactor: 常量

--- src/main/java/icu/yeguo/cloudnest/common/Response.java
+++ src/main/java/icu/yeguo/cloudnest/common/Response.java
@@ -1,11 +1,13 @@
 package icu.yeguo.cloudnest.common;
 
-import icu.yeguo.cloudnest.constant.HttpStatus;
+import icu.yeguo.cloudnest.constant.HttpStatusConstant;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
+
+import static icu.yeguo.cloudnest.constant.HttpStatusConstant.OK;
 
 @NoArgsConstructor
 @Data
@@ -24,7 +26,7 @@ public class Response<T> implements Serializable {
     }
 
     public static <T> Response<T> success(T data) {
-        return new Response<>(200, data, "success");
+        return new Response<>(OK, data, "success");
     }
 
     public static <T> Response<T> error(int code, String message) {
@@ -32,6 +34,6 @@ public class Response<T> implements Serializable {
     }
 
     public static <T> Response<T> error(String message) {
-        return new Response<>(HttpStatus.InternalServerError,null, message);
+        return new Response<>(HttpStatusConstant.InternalServerError,null, message);
     }
 }
