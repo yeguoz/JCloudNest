163ee6233da7e032046be329c8e66a8ae4716809
yeguoz <aidjajd@163.com>
2025-01-04 22:52:29 +0800

    feat: 设置常量

---
+++ src/main/java/icu/yeguo/cloudnest/constant/SettingConstant.java
@@ -0,0 +1,7 @@
+package icu.yeguo.cloudnest.constant;
+
+public interface SettingConstant {
+    String REGISTER_ENABLED = "register_enabled";
+    String FALSE = "false";
+    String REGISTER_GROUP = "register_group";
+}
