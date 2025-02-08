52a0be6e2fc4b25159d7772708a94fcc88ed4e05
yeguoz <aidjajd@163.com>
2025-02-08 20:17:00 +0800

    feat: swagger信息

--- src/main/java/icu/yeguo/cloudnest/model/dto/UserRegisterDTO.java
+++ src/main/java/icu/yeguo/cloudnest/model/dto/UserRegisterDTO.java
@@ -3,10 +3,13 @@ package icu.yeguo.cloudnest.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-@Schema(name = "UserRegisterDTO",description = "用户注册数据")
+@Schema(name = "UserRegisterDTO",description = "用户注册数据",requiredProperties={"email","password","checkPassword"})
 @Data
 public class UserRegisterDTO {
+    @Schema(description = "邮箱", example = "123@163.com")
     private String email;
+    @Schema(description = "密码")
     private String password;
+    @Schema(description = "重复密码")
     private String checkPassword;
 }
