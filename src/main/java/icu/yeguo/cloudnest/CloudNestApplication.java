4097bd7884916bc85be7d5489811005f69d176c4
yeguoz <aidjajd@163.com>
2025-02-08 20:15:25 +0800

    feat: @ServletComponentScan

--- src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
+++ src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
@@ -3,8 +3,10 @@ package icu.yeguo.cloudnest;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("icu.yeguo.cloudnest.mapper")
 public class CloudNestApplication {
 
