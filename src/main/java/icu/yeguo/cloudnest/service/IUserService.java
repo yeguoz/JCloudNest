2785a87156fc1e9a86f7675436d449a231570993
yeguoz <aidjajd@163.com>
2025-02-08 20:13:37 +0800

    feat: login

--- src/main/java/icu/yeguo/cloudnest/service/IUserService.java
+++ src/main/java/icu/yeguo/cloudnest/service/IUserService.java
@@ -1,15 +1,18 @@
 package icu.yeguo.cloudnest.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import icu.yeguo.cloudnest.model.dto.UserLoginDTO;
 import icu.yeguo.cloudnest.model.dto.UserRegisterDTO;
 import icu.yeguo.cloudnest.model.entity.User;
 import icu.yeguo.cloudnest.model.vo.UserVO;
+import jakarta.servlet.http.HttpSession;
 
 /**
- *
  * @author yeguo
  * @since 2024-12-31
  */
 public interface IUserService extends IService<User> {
-    UserVO createUser(UserRegisterDTO userRegisterDTO);
+    UserVO createUser(HttpSession session,UserRegisterDTO userRegisterDTO, String captcha);
+
+    UserVO login(HttpSession session, UserLoginDTO userLoginDTO, String captcha);
 }
