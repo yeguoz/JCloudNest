6e8541b1da7c9a31a04801c9966655e309db49d2
yeguoz <aidjajd@163.com>
2025-02-16 15:24:50 +0800

    feat: 启用事务管理

--- src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
+++ src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("icu.yeguo.cloudnest.mapper")
