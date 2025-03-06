4bc50dda5a5e49393c96592ddad2904f64395fdb
yeguoz <aidjajd@163.com>
2025-03-06 12:04:06 +0800

    feat: 常量

--- src/main/java/icu/yeguo/cloudnest/constant/CommonConstant.java
+++ src/main/java/icu/yeguo/cloudnest/constant/CommonConstant.java
@@ -1,5 +1,17 @@
 package icu.yeguo.cloudnest.constant;
 
 public interface CommonConstant {
+    String TRUE = "true";
+    String FALSE = "false";
     String SUCCESS = "success";
+    String EMAIL_REGEX = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
+    String CAPTCHA = "captcha";
+    String SESSION_CAPTCHA_REGISTER = "sessionCaptchaRegister";
+    String SESSION_CAPTCHA_REGISTER_MAX_AGE = "sessionCaptchaRegisterMaxAge";
+    String SESSION_CAPTCHA_LOGIN = "sessionCaptchaLogin";
+    String SESSION_CAPTCHA_LOGIN_MAX_AGE = "sessionCaptchaLoginMaxAge";
+    String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
+    String BASE64_IMAGE_PREFIX = "data:image/png;base64,";
+    String ROOT = "/";
+    String EMPTY = "";
 }
