25d27852f0e99a9c62d2054af349d1d004ca631f
yeguoz <aidjajd@163.com>
2025-03-06 12:00:38 +0800

    feat: createFolder

--- src/main/java/icu/yeguo/cloudnest/service/IFolderService.java
+++ src/main/java/icu/yeguo/cloudnest/service/IFolderService.java
@@ -3,11 +3,12 @@ package icu.yeguo.cloudnest.service;
 import icu.yeguo.cloudnest.model.entity.Folder;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.io.IOException;
+
 /**
  * @author yeguo
  * @createDate 2025-02-15 18:13:03
  */
 public interface IFolderService extends IService<Folder> {
-
-    Long createFolder(Integer userId, String path, String name);
+    Long createFolder(Integer userId, String path, String name) throws IOException;
 }
