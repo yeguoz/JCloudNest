2ff30e9debf75eb7b12cf119bb382b9dd9a046a2
yeguoz <aidjajd@163.com>
2025-05-11 00:45:05 +0800

    feat: SettingConstant

--- src/main/java/icu/yeguo/cloudnest/constant/SettingConstant.java
+++ src/main/java/icu/yeguo/cloudnest/constant/SettingConstant.java
@@ -1,7 +1,9 @@
 package icu.yeguo.cloudnest.constant;
 
 public interface SettingConstant {
-    String REGISTER_ENABLED = "register_enabled";
-    String FALSE = "false";
-    String REGISTER_GROUP = "register_group";
+    String REGISTER_ENABLED = "registerEnabled";
+    String REGISTER_GROUP = "registerGroup";
+    String REGISTER_CAPTCHA = "registerCaptcha";
+    String LOGIN_CAPTCHA = "loginCaptcha";
+    String AUTH = "auth";
 }
