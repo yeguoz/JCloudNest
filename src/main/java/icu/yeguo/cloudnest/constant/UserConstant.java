fd07cf903855a40e7c0fc3bacef0a447bd40638f
yeguoz <aidjajd@163.com>
2025-05-11 00:44:51 +0800

    feat: UserConstant

--- src/main/java/icu/yeguo/cloudnest/constant/UserConstant.java
+++ src/main/java/icu/yeguo/cloudnest/constant/UserConstant.java
@@ -2,4 +2,9 @@ package icu.yeguo.cloudnest.constant;
 
 public interface UserConstant {
     int NORMAL = 0;
+    int NOT_ACTIVE = 1;
+    int BANNED = 2;
+    String USER_VO = "userVO";
+    String ADMIN = "Admin";
+    String USER = "User";
 }
