e250f63fd7b4352be9bd06eb5b0f2a4bd3eb71d1
yeguoz <aidjajd@163.com>
2025-05-11 00:45:30 +0800

    feat: PlaceholderConstant

---
+++ src/main/java/icu/yeguo/cloudnest/constant/PlaceholderConstant.java
@@ -0,0 +1,13 @@
+package icu.yeguo.cloudnest.constant;
+
+public interface PlaceholderConstant {
+    String UID_PLACEHOLDER = "{uid}";
+    String UUID_PLACEHOLDER = "{uuid}";
+    String FINGERPRINT_PLACEHOLDER  = "{fingerprint}";
+    String HASH_PLACEHOLDER = "{hash}";
+    String HASH2_PLACEHOLDER = "{hash2}";
+    String INDEX_PLACEHOLDER = "{index}";
+    String UPLOAD_ID_PLACEHOLDER = "{uploadId}";
+    String FILENAME_PLACEHOLDER = "{filename}";
+    String LINK_PLACEHOLDER = "${link}";
+}
