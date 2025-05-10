6fa1dd9ad4112d3c3f3bd7aa33afa4c81d12381c
yeguoz <aidjajd@163.com>
2025-05-11 00:45:59 +0800

    feat: CommonConstant

--- src/main/java/icu/yeguo/cloudnest/constant/CommonConstant.java
+++ src/main/java/icu/yeguo/cloudnest/constant/CommonConstant.java
@@ -10,8 +10,10 @@ public interface CommonConstant {
     String SESSION_CAPTCHA_REGISTER_MAX_AGE = "sessionCaptchaRegisterMaxAge";
     String SESSION_CAPTCHA_LOGIN = "sessionCaptchaLogin";
     String SESSION_CAPTCHA_LOGIN_MAX_AGE = "sessionCaptchaLoginMaxAge";
-    String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
     String BASE64_IMAGE_PREFIX = "data:image/png;base64,";
     String ROOT = "/";
-    String EMPTY = "";
+    String FOLDER = "folder";
+    String FILE = "file";
+    String DOT_PART = ".part";
+    String DOT = ".";
 }
