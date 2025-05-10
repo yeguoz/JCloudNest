a9df5b8763b6cfadf9f76583be7e1a1321b8d194
yeguoz <aidjajd@163.com>
2025-05-11 00:53:31 +0800

    feat: SettingVO

---
+++ src/main/java/icu/yeguo/cloudnest/model/vo/SettingVO.java
@@ -0,0 +1,11 @@
+package icu.yeguo.cloudnest.model.vo;
+
+import lombok.Data;
+
+@Data
+public class SettingVO {
+    private Integer id;
+    private String type;
+    private String name;
+    private String value;
+}
