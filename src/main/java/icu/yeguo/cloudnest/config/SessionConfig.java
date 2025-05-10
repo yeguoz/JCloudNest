87c11f755a060b662070eb20861f25f16a91ec85
yeguoz <aidjajd@163.com>
2025-05-11 00:49:38 +0800

    refactor: setCookieMaxAge

--- src/main/java/icu/yeguo/cloudnest/config/SessionConfig.java
+++ src/main/java/icu/yeguo/cloudnest/config/SessionConfig.java
@@ -14,20 +14,19 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400, redisNamespace = "cloudnest:session")
 public class SessionConfig {
     private final static String CLOUDNEST_SESSION_ID = "cloudnest-sessionId";
-    // 将session序列化改为JSON
+
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
-    // 配置cookie
+
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setCookieName(CLOUDNEST_SESSION_ID);
         serializer.setUseHttpOnlyCookie(true);
-        serializer.setCookieMaxAge(86400);
         return serializer;
     }
 }
