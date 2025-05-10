ee7def82b77a794a487b4eff1aee7c29e2f4de3b
yeguoz <aidjajd@163.com>
2025-05-11 00:44:37 +0800

    feat: UploadConstant

---
+++ src/main/java/icu/yeguo/cloudnest/constant/UploadConstant.java
@@ -0,0 +1,14 @@
+package icu.yeguo.cloudnest.constant;
+
+public interface UploadConstant {
+    String PENDING = "pending";
+    String PROCESSING = "processing";
+    String COMPLETED = "completed";
+    String STATUS = "status";
+    String TOTAL_CHUNKS = "totalChunks";
+    String CHUNK_DIR_PATH = "chunkDirPath";
+    String FINGERPRINT = "fingerprint";
+    String UPLOAD_ID = "uploadId";
+    String FILENAME = "filename";
+    String FILE_SIZE = "fileSize";
+}
