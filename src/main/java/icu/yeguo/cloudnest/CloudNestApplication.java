2da435b20c07c66c95c1bb98101e76acd5389eac
yeguoz <aidjajd@163.com>
2025-05-11 00:31:19 +0800

    feat: @EnableScheduling

--- src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
+++ src/main/java/icu/yeguo/cloudnest/CloudNestApplication.java
@@ -3,17 +3,17 @@ package icu.yeguo.cloudnest;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @EnableTransactionManagement
 @SpringBootApplication
-@ServletComponentScan
 @MapperScan("icu.yeguo.cloudnest.mapper")
 public class CloudNestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudNestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNestApplication.class, args);
+    }
 
 }
