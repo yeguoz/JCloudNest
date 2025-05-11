799d326f802c9c43c22a9a0f2c968e3eca7bbba4
yeguoz <aidjajd@163.com>
2025-05-11 13:48:12 +0800

    feat: ThreadPoolConfig

---
+++ src/main/java/icu/yeguo/cloudnest/config/ThreadPoolConfig.java
@@ -0,0 +1,23 @@
+package icu.yeguo.cloudnest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.*;
+
+@Configuration
+public class ThreadPoolConfig {
+    @Bean
+    public Executor threadPoolExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(2);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("Async-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
