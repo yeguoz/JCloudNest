3283ce23f452bae7cf5bb9457e468d48ce752061
yeguoz <aidjajd@163.com>
2025-05-11 14:03:31 +0800

    feat: renameFolder、queryFolderIdByRelativePath、findFolderId、copyFolderRecursively、getOneByParentIdAndName、createFolderEntity、deleteFolderRecursively、removePhysicallyById

--- src/main/java/icu/yeguo/cloudnest/service/IFolderService.java
+++ src/main/java/icu/yeguo/cloudnest/service/IFolderService.java
@@ -1,7 +1,9 @@
 package icu.yeguo.cloudnest.service;
 
+import icu.yeguo.cloudnest.model.dto.UploadRequest;
 import icu.yeguo.cloudnest.model.entity.Folder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import icu.yeguo.cloudnest.model.vo.UserVO;
 
 import java.io.IOException;
 
@@ -10,5 +12,21 @@ import java.io.IOException;
  * @createDate 2025-02-15 18:13:03
  */
 public interface IFolderService extends IService<Folder> {
-    Long createFolder(Integer userId, String path, String name) throws IOException;
+    Long createFolder(UserVO userVO, String path, String name) throws IOException;
+
+    int renameFolder(Long id, String name);
+
+    Long queryFolderIdByRelativePath(Long parentId, String relativePath);
+
+    long findFolderId(int userId, String path);
+
+    void copyFolderRecursively(UserVO userVO, Long originFolderId, Long targetFolderId, String folderName, UploadRequest uploadRequest);
+
+    Folder getOneByParentIdAndName(Long targetFolderId, String folderName);
+
+    Folder createFolderEntity(Integer userId, String name, Long parentId);
+
+    void deleteFolderRecursively(UserVO userVO, Long originFolderId);
+
+    Boolean removePhysicallyById(Long originFolderId);
 }
--- src/main/java/icu/yeguo/cloudnest/service/impl/FolderServiceImpl.java
+++ src/main/java/icu/yeguo/cloudnest/service/impl/FolderServiceImpl.java
@@ -3,13 +3,19 @@ package icu.yeguo.cloudnest.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import icu.yeguo.cloudnest.exception.BusinessException;
+import icu.yeguo.cloudnest.model.dto.UploadRequest;
 import icu.yeguo.cloudnest.model.entity.Folder;
-import icu.yeguo.cloudnest.service.CommonService;
-import icu.yeguo.cloudnest.service.IFolderService;
+import icu.yeguo.cloudnest.model.vo.UserVO;
+import icu.yeguo.cloudnest.service.*;
 import icu.yeguo.cloudnest.mapper.FolderMapper;
+import icu.yeguo.cloudnest.util.FolderHelper;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+import static icu.yeguo.cloudnest.constant.CommonConstant.ROOT;
 
 /**
  * @author yeguo
@@ -19,14 +25,15 @@ import org.springframework.stereotype.Service;
 public class FolderServiceImpl extends ServiceImpl<FolderMapper, Folder>
         implements IFolderService {
 
-    @Autowired
+    @Resource
     private FolderMapper folderMapper;
-    @Autowired
-    private CommonService commonService;
+    @Resource
+    private FolderHelper folderHelper;
 
     @Override
-    public Long createFolder(Integer userId, String path, String name) {
-        long parentFolderId = commonService.findFolderId(userId, path);
+    public Long createFolder(UserVO userVO, String path, String name) {
+        int userId = userVO.getId();
+        long parentFolderId = this.findFolderId(userId, path);
         if (parentFolderId == 0)
             throw new BusinessException(HttpServletResponse.SC_BAD_REQUEST, "父文件夹不存在");
         // 不能有同名文件夹
@@ -47,8 +54,89 @@ public class FolderServiceImpl extends ServiceImpl<FolderMapper, Folder>
         return folder.getId();
     }
 
+    @Override
+    public int renameFolder(Long id, String name) {
+        Folder folder = folderMapper.selectById(id);
+        if (folder == null)
+            throw new BusinessException(HttpServletResponse.SC_BAD_REQUEST, "文件夹不存在");
+        folder.setName(name);
+        return folderMapper.updateById(folder);
+    }
+
+    @Override
+    public Long queryFolderIdByRelativePath(Long parentId, String relativePath) {
+        String[] relativePaths = Arrays.copyOfRange(relativePath.split(ROOT), 1, relativePath.split(ROOT).length);
+        for (String path : relativePaths) {
+            if (parentId < 1)
+                throw new BusinessException(HttpServletResponse.SC_BAD_REQUEST, "文件夹不存在");
+            Folder folder = folderMapper.selectOne(new LambdaQueryWrapper<Folder>()
+                    .eq(Folder::getParentId, parentId)
+                    .eq(Folder::getName, path));
+            parentId = folder != null ? folder.getId() : 0;
+        }
+        return parentId;
+    }
+
+    @Override
+    public long findFolderId(int userId, String path) {
+        String[] paths;
+        if (ROOT.equals(path)) {
+            paths = new String[1];
+        } else {
+            paths = path.split(ROOT);
+        }
+        paths[0] = ROOT;
+
+        long folderId = 0;
+        for (String item : paths) {
+            if (ROOT.equals(item)) {
+                Folder folder = folderMapper.selectOne(new LambdaQueryWrapper<Folder>()
+                        .eq(Folder::getUserId, userId)
+                        .eq(Folder::getName, item)
+                        .isNull(Folder::getParentId));
+                folderId = folder != null ? folder.getId() : 0;
+            } else {
+                if (folderId == 0)
+                    break;
+                Folder folder = folderMapper.selectOne(new LambdaQueryWrapper<Folder>()
+                        .eq(Folder::getUserId, userId)
+                        .eq(Folder::getName, item)
+                        .eq(Folder::getParentId, folderId));
+                folderId = folder != null ? folder.getId() : 0;
+            }
+        }
+        return folderId;
+    }
+
+    @Override
+    public void copyFolderRecursively(UserVO userVO, Long currentFolderId, Long targetFolderId, String folderName,
+                                      UploadRequest uploadRequest) {
+        folderHelper.copyFolderRecursively(userVO, currentFolderId, targetFolderId, folderName, uploadRequest);
+    }
+
+    @Override
+    public Folder getOneByParentIdAndName(Long parentId, String name) {
+        return this.getOne(new LambdaQueryWrapper<Folder>()
+                .eq(Folder::getParentId, parentId).eq(Folder::getName, name));
+    }
+
+    @Override
+    public Folder createFolderEntity(Integer userId, String name, Long parentId) {
+        Folder folder = new Folder();
+        folder.setUserId(userId);
+        folder.setName(name);
+        folder.setParentId(parentId);
+        this.save(folder);
+        return folder;
+    }
+
+    @Override
+    public void deleteFolderRecursively(UserVO userVO, Long originFolderId) {
+        folderHelper.deleteFolderRecursively(userVO, originFolderId);
+    }
+
+    @Override
+    public Boolean removePhysicallyById(Long originFolderId) {
+        return folderMapper.removePhysicallyById(originFolderId);
+    }
 }
-
-
-
-
