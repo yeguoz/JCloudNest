f9305456d743de93c2a7a70e3034af472d946416
yeguoz <aidjajd@163.com>
2025-05-12 16:57:54 +0800

    refactor: RedissonConfig

--- src/main/java/icu/yeguo/cloudnest/config/RedissonConfig.java
+++ src/main/java/icu/yeguo/cloudnest/config/RedissonConfig.java
@@ -3,18 +3,33 @@ package icu.yeguo.cloudnest.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
+        String address = String.format("redis://%s:6379", redisHost);
+
         Config config = new Config();
+        if (redisPassword.isEmpty()){
+            config.useSingleServer()
+                    .setAddress(address)
+                    .setDatabase(0);
+            return Redisson.create(config);
+        }
         config.useSingleServer()
-              .setAddress("redis://127.0.0.1:6379")
-              .setDatabase(0);
+                .setAddress(address)
+                .setPassword(redisPassword)
+                .setDatabase(0);
 
         return Redisson.create(config);
     }
